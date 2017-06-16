699cc32204f477477b20c1e6ecef5abd4a564230
dev.yuriy.n@gmail.com <dev.yuriy.n@gmail.com>
2017-06-16 14:31:21 +0300

    refactoring and iterface improvements

--- Neleus.LambdaCompare.Tests/Tests.cs
+++ Neleus.LambdaCompare.Tests/Tests.cs
@@ -11,81 +11,53 @@ namespace Neleus.LambdaCompare.Tests
         [TestMethod]
         public void BasicConst()
         {
-            var f1 = GetBasicExpr1();
-            var f2 = GetBasicExpr2();
+            var const1 = 25;
+            var f1 = (Expression<Func<int, string, string>>) ((first, second) =>
+                $"{(first + const1).ToString(CultureInfo.InvariantCulture)}{first + second}{"some const value".ToUpper()}{const1}");
+
+            var const2 = "some const value";
+            var const3 = "{0}{1}{2}{3}";
+            var f2 = (Expression<Func<int, string, string>>) ((i, s) =>
+                string.Format(const3, (i + 25).ToString(CultureInfo.InvariantCulture), i + s, const2.ToUpper(), 25));
+
             Assert.IsTrue(Lambda.Eq(f1, f2));
         }
 
         [TestMethod]
         public void PropAndMethodCall()
         {
-            var f1 = GetPropAndMethodExpr1();
-            var f2 = GetPropAndMethodExpr2();
+            var f1 = (Expression<Func<Uri, bool>>) (arg1 => Uri.IsWellFormedUriString(arg1.ToString(), UriKind.Absolute));
+            var f2 = (Expression<Func<Uri, bool>>) (u => Uri.IsWellFormedUriString(u.ToString(), UriKind.Absolute));
+
             Assert.IsTrue(Lambda.Eq(f1, f2));
         }
 
         [TestMethod]
         public void MemberInitWithConditional()
         {
-            var f1 = GetMemberInitExpr1();
-            var f2 = GetMemberInitExpr2();
+            var port = 443;
+            var f1 = (Expression<Func<Uri, UriBuilder>>) (x => new UriBuilder(x)
+            {
+                Port = port,
+                Host = string.IsNullOrEmpty(x.Host) ? "abc" : "def"
+            });
+
+            var isSecure = true;
+            var f2 = (Expression<Func<Uri, UriBuilder>>) (u => new UriBuilder(u)
+            {
+                Host = string.IsNullOrEmpty(u.Host) ? "abc" : "def",
+                Port = isSecure ? 443 : 80
+            });
+
             Assert.IsTrue(Lambda.Eq(f1, f2));
         }
 
-        [TestMethod]
+        [TestMethod, Ignore]
         public void AnonymousType()
         {
-            var f1 = GetAnonymousExpr1();
-            var f2 = GetAnonymousExpr2();
+            var f1 = (Expression<Func<Uri, object>>) (x => new { Port = 443, x.Host, Addr = x.AbsolutePath });
+            var f2 = (Expression<Func<Uri, object>>) (u => new { u.Host, Port = 443, Addr = u.AbsolutePath });
             Assert.Inconclusive("Anonymous Types are not supported");
-        }
-
-        private static Expression<Func<int, string, string>> GetBasicExpr2()
-        {
-            var const2 = "some const value";
-            var const3 = "{0}{1}{2}{3}";
-            return (i, s) =>
-                string.Format(const3, (i + 25).ToString(CultureInfo.InvariantCulture), i + s, const2.ToUpper(), 25);
-        }
-
-        private static Expression<Func<int, string, string>> GetBasicExpr1()
-        {
-            var const1 = 25;
-            return (first, second) =>
-                string.Format("{0}{1}{2}{3}", (first + const1).ToString(CultureInfo.InvariantCulture), first + second,
-                    "some const value".ToUpper(), const1);
-        }
-
-        private static Expression<Func<Uri, bool>> GetPropAndMethodExpr2()
-        {
-            return u => Uri.IsWellFormedUriString(u.ToString(), UriKind.Absolute);
-        }
-
-        private static Expression<Func<Uri, bool>> GetPropAndMethodExpr1()
-        {
-            return arg1 => Uri.IsWellFormedUriString(arg1.ToString(), UriKind.Absolute);
-        }
-
-        private static Expression<Func<Uri, UriBuilder>> GetMemberInitExpr2()
-        {
-            var isSecure = true;
-            return u => new UriBuilder(u) { Host = string.IsNullOrEmpty(u.Host) ? "abc" : "def", Port = isSecure ? 443 : 80 };
-        }
-
-        private static Expression<Func<Uri, UriBuilder>> GetMemberInitExpr1()
-        {
-            var port = 443;
-            return x => new UriBuilder(x) { Port = port, Host = string.IsNullOrEmpty(x.Host) ? "abc" : "def" };
-        }
-
-        private static Expression<Func<Uri, object>> GetAnonymousExpr2()
-        {
-            return u => new { u.Host, Port = 443, Addr = u.AbsolutePath };
-        }
-
-        private static Expression<Func<Uri, object>> GetAnonymousExpr1()
-        {
-            return x => new { Port = 443, x.Host, Addr = x.AbsolutePath };
         }
     }
 }
--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 This project compares two lambda expressions recursively traversing their trees.
 
 It doesn''t use strict expression tree comparison. Instead, it collapses constant expressions and compares their values rather than their trees. It is useful for mocks validation when the lambda has a reference to local variable. In his case the variable is compared by its value.
+See unit tests for examples.
 
-See origin https://stackoverflow.com/a/24528357/2528649
+See also origin https://stackoverflow.com/a/24528357/2528649
